d7cdb610ad122de239b389c48cb8f3b033f77807
Baktygul <baktygul160799@gmail.com>
2024-10-26 21:20:43 +0600

    refactoring

--- lib/example_widget.dart
+++ lib/example_widget.dart
@@ -4,32 +4,70 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+class User {
+  final int age;
+  User(this.age);
+
+  User copyWith({
+    int? age,
+  }) {
+    return User(age ?? this.age);
+  }
+}
+
+class UserService {
+  var _user = User(0);
+  User get user => _user;
+
+  Future<void> loadValue() async {
+    final sharedPrefernces = await SharedPreferences.getInstance();
+    final age = sharedPrefernces.getInt('age') ?? 0;
+    _user = User(age);
+  }
+
+  Future<void> saveValue() async {
+    final sharedPrefernces = await SharedPreferences.getInstance();
+    sharedPrefernces.setInt('age', _user.age);
+  }
+
+  incrementAge() {
+    _user = _user.copyWith(age: _user.age + 1);
+  }
+
+  decrementAge() {
+    _user = _user.copyWith(age: max(_user.age - 1, 0));
+  }
+}
+
+class ViewModelService {
+  final String ageTitle;
+  ViewModelService({required this.ageTitle});
+}
+
 class ViewModel extends ChangeNotifier {
+  final _userService = UserService();
+  var _state = ViewModelService(ageTitle: '');
+  ViewModelService get state => _state;
+
+  Future<void> loadValue() async {
+    await _userService.loadValue();
+    _state = ViewModelService(ageTitle: _userService.user.age.toString());
+    notifyListeners();
+  }
+
   ViewModel() {
     loadValue();
   }
 
-  var _age = 0;
-  int get age => _age;
-  void loadValue() async {
-    final sharedPrefernces = await SharedPreferences.getInstance();
-    _age = sharedPrefernces.getInt('age') ?? 0;
+  Future<void> onIncBtnPressed() async {
+    _userService.incrementAge();
+    _state = ViewModelService(ageTitle: _userService.user.age.toString());
     notifyListeners();
   }
 
-  Future<void> incrementAge() async {
-    _age++;
-    final sharedPrefernces = await SharedPreferences.getInstance();
-    sharedPrefernces.setInt('age', _age);
-
-    notifyListeners();
-  }
-
-  Future<void> decrementAge() async {
-    _age = max(_age - 1, 0);
-    final sharedPrefernces = await SharedPreferences.getInstance();
-    sharedPrefernces.setInt('age', _age);
-
+  Future<void> onDecBtnPressed() async {
+    _userService.decrementAge();
+    _state = ViewModelService(ageTitle: _userService.user.age.toString());
     notifyListeners();
   }
 }
@@ -62,8 +100,8 @@ class _AgeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _age = context.select((ViewModel vm) => vm.age);
-    return Text("$_age");
+    final age = context.select((ViewModel vm) => vm.state.ageTitle);
+    return Text(age);
   }
 }
 
@@ -74,7 +112,7 @@ class AgeIncWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final viewModel = context.read<ViewModel>();
     return ElevatedButton(
-        onPressed: viewModel.incrementAge, child: const Text("+"));
+        onPressed: viewModel.onIncBtnPressed, child: const Text("+"));
   }
 }
 
@@ -85,6 +123,6 @@ class AgeDecWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final viewModel = context.read<ViewModel>();
     return ElevatedButton(
-        onPressed: viewModel.decrementAge, child: const Text("-"));
+        onPressed: viewModel.onDecBtnPressed, child: const Text("-"));
   }
 }
